7f27c1f35f5d2ce54eb379f5f574ea4336e5aa70
Russell M <russellmacquarrie2022@u.northwestern.edu>
2022-02-03 18:28:07 -0600

    Wrote some ugly code
    Stage 1 passes, but stage 2 is stuck on 778

--- streamer.py
+++ streamer.py
@@ -5,7 +5,11 @@ from socket import INADDR_ANY
 import struct
 from queue import PriorityQueue
 
+
 class Streamer:
+    seq_num = 0
+    expected_seq_num = 0
+    recv_buffer = {}
     def __init__(self, dst_ip, dst_port,
                  src_ip=INADDR_ANY, src_port=0):
         """Default values listen on all network interfaces, chooses a random source port,
@@ -18,13 +22,13 @@ class Streamer:
     def send(self, data_bytes: bytes) -> None:
         """Note that data_bytes can be larger than one packet."""
         chunks = []
-        seq_num = 0
         # for b in data_bytes:
-        #     value = (seq_num, b)
-        #     arg = 'I ' + str(len(b)) + 's'
-        #     s = struct.Struct(arg)
-        #     b = s.pack(*value)
-        #     seq_num += 1
+            # value = (seq_num, b)
+            # arg = 'I ' + str(len(b)) + 's'
+            # s = struct.Struct(arg)
+            # b = s.pack(*value)
+            # seq_num += 1
+        
 
         while len(data_bytes) > 0:
             if len(data_bytes) > 500:
@@ -35,14 +39,13 @@ class Streamer:
                 data_bytes = []
 
         sequenced_chunks = []
-        seq_num = 0
 
         for c in chunks:
-            value = (seq_num, c)
+            value = (Streamer.seq_num, c)
             arg = 'I ' + str(len(c)) + 's'
             s = struct.Struct(arg)
             sequenced_chunks.append(s.pack(*value))
-            seq_num += 1
+            Streamer.seq_num += 1
 
         for c in sequenced_chunks:
             # print('SENDING: ' + str(c))
@@ -51,26 +54,39 @@ class Streamer:
     def recv(self) -> bytes:
         """Blocks (waits) if no data is ready to be read from the connection."""
 
-        recv_buffer = {}
+        
 
         data, addr = self.socket.recvfrom()
         # print('RECEIVING: ' + str(data))
         # for i in range(len(data)):
         #     print(data[i])
 
-        expected_seq_num = 0
-
+        
+        
         arg = 'I ' + str(len(data) - 4) + 's'
         data = struct.unpack(arg, data)
         seq_number = data[0]
         payload = data[1]
+        output = b''
 
-        if seq_number == expected_seq_num:
-            expected_seq_num += 1
-            return payload
+        #print(payload)
+        while seq_number != Streamer.expected_seq_num:
+            
+            Streamer.recv_buffer[seq_number] = payload
+            if Streamer.expected_seq_num in Streamer.recv_buffer:
+                output += Streamer.recv_buffer[Streamer.expected_seq_num]
+                del Streamer.recv_buffer[Streamer.expected_seq_num]
+                Streamer.expected_seq_num += 1
+            else:
+                break
+            
+            
+        #print(recv_buffer)        
+        if seq_number == Streamer.expected_seq_num:
+            Streamer.expected_seq_num += 1
+            output += payload
         # else if dictionary contains expected, return that and remove from dict, expected ++, add current to dict
-        else:
-            recv_buffer[seq_number] = payload
+        return output
 
     # {3, 4, 5, 6, 8, 9, 10}
 
@@ -78,7 +94,6 @@ class Streamer:
 
 
         # For now, I'll just pass the full UDP payload to the app
-        return payload
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
